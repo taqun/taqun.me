300dc2521b007e712d42a627a42bdcc7540b402f
taqun <694558+taqun@users.noreply.github.com>
2023-03-20 01:30:47 +0900

    Fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,9 @@ import { NextPage } from 'next';
 import { AppProps } from 'next/app';
 import { ReactElement, ReactNode } from 'react';
 
-import '@/styles/common.scss';
 import GTM from '@/components/GTM';
+
+import '@/styles/common.scss';
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
